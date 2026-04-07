8ec220619792ed1dff2022d35794bfae05156256
Aquiii <dareakintundealamu@gmail.com>
2026-04-07 15:43:28 +0100

    Route protection, tired

--- Server/routes/routes.js
+++ Server/routes/routes.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser } = require('../controllers/Authcontroller');
+const { protect } = require('../middleware/authMiddleware'); 
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+
+router.get('/profile', protect, async (req, res) => {
+
+  res.json({
+    _id: req.user._id,
+    name: req.user.name,
+    email: req.user.email,
+    role: req.user.role
+  });
+});
 
 module.exports = router;
